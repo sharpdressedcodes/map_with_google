779e4d49b3fa6ebb3c4b49c490e6dba1a09d8e4b
sharpdressedcodes <spam@sharpdressedcodes.com>
2013-03-29 23:14:01 +1100

    Correct issues in email from Mozilla to get full approval

---
+++ chrome/content/suricattagmap/main.js
@@ -0,0 +1,34 @@
+
+var suricattagmap = new Object();
+
+suricattagmap.locateOnGMap = function(){
+
+	var textSelection=content.document.getSelection();
+	//var cleanedSelection=textSelection.replace(/\s/g,' ').replace(/  {2,}/g,' ').replace(/ $|^ /g,'');
+
+	if (textSelection!=''){
+		var url='http://maps.google.com/maps?q='+escape(textSelection);
+
+		/*
+		 relatedToCurrent:true means the new tab will open up
+		 straight after the current tab - remove to open at end of tabs
+		*/
+
+		var tab = getBrowser().addTab(url,{relatedToCurrent:true});
+		getBrowser().selectedTab = tab;
+	}
+}
+
+window.addEventListener("load", suricattagmap.gmapInit, true);
+
+// hides the item when appropriate (use same logic as for Back, Stop etc.)
+// script written by Stephen Clavering, used in his goHome extension
+
+suricattagmap.gmapInit = function() {
+  document.getElementById("contentAreaContextMenu").addEventListener("popupshowing",suricattagmap.gmapHide,false);
+}
+
+suricattagmap.gmapHide = function() {
+  var cm = gContextMenu;
+  document.getElementById("suricattagmap").hidden = !cm.isTextSelected;
+}
