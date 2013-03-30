0b136af939fc5d9031e6960764bbda35d0408137
sharpdressedcodes <spam@sharpdressedcodes.com>
2013-03-31 00:04:39 +1100

    Dont show menuItem if no text selected and append selected text to menuItem label

--- chrome/content/suricattagmap/main.js
+++ chrome/content/suricattagmap/main.js
@@ -1,6 +1,8 @@
 
 var suricattagmap = new Object();
 
+suricattagmap.default_menutext='';
+
 suricattagmap.locateOnGMap = function(){
 
   var textSelection=content.document.getSelection().toString();
@@ -21,11 +23,28 @@ suricattagmap.locateOnGMap = function(){
 
 window.addEventListener("load", suricattagmap.gmapInit, true);
 
+window.addEventListener("contextmenu", function(e) {
+    var menu = document.getElementById('suricattagmap');
+
+    if (suricattagmap.default_menutext=='')
+      suricattagmap.default_menutext=menu.getAttribute('label');
+
+    var textSelection=content.document.getSelection().toString();
+    menu.hidden=(textSelection.length==0);
+
+    if (!menu.hidden && suricattagmap.default_menutext!='')
+      menu.setAttribute('label',suricattagmap.default_menutext+'"'+textSelection+'"');
+
+}, false);
+
 // hides the item when appropriate (use same logic as for Back, Stop etc.)
 // script written by Stephen Clavering, used in his goHome extension
 
 suricattagmap.gmapInit = function() {
   document.getElementById("contentAreaContextMenu").addEventListener("popupshowing",suricattagmap.gmapHide,false);
+  var menu=document.getElementById('suricattagmap');
+  if (suricattagmap.default_menutext=='')
+    suricattagmap.default_menutext=menu.getAttribute('label');
 }
 
 suricattagmap.gmapHide = function() {
